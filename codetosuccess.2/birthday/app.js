b3cb3010fdd8aaac630e7af7375539d96d3706a7
Emily433 <emilypmortensen@gmail.com>
2018-07-10 11:38:08 -0600

    amess

--- codetosuccess.2/birthday/app.js
+++ codetosuccess.2/birthday/app.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>DevCamp React Starter</h1>
+      <div className="grid">
+      <div className="grid_skew- one-box"></div>
+      <div className="grid_skew- two"></div>
+      <div className="grid_skew- three"></div>
       </div>
     );
   }
