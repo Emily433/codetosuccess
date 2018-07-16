3def36b72c42cae53a83e20195d719bd2daf97c9
Emily433 <emilypmortensen@gmail.com>
2018-07-16 08:55:16 -0600

    scheduler

--- Schedule/home.js
+++ Schedule/home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="home">
-        <h1>Course Scheduler</h1>
-      </div>
-    );
-  }
+import Library from './library/library';
+import Schedule from './schedule/schedule';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <Library/>
+                <Schedule/>
+            </div>
+        );
+    }
 }
+
+export default Home;
---
+++ Schedule/library.js
@@ -0,0 +1,13 @@
+class Library extends Component {
+    render() {
+        return (
+            <div className="library">
+            <h1 className="library_title">course library</h1>
+
+
+            </div>
+        )
+    }
+}
+
+export default Library;
