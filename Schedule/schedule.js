f7d2716c4d2a58214db611c6e29019819a393f41
Emily433 <emilypmortensen@gmail.com>
2018-07-16 09:10:59 -0600

    masschedule

--- Schedule/schedule.js
+++ Schedule/schedule.js
@@ -1,8 +1,13 @@
+import React,{Commponent} from 'react';
+
+import Gradient from '../src/components/gradient';
 class Schedule extends Commponent {
     render() {
         return (
             <div className="schedule">
-                <h1 className="schedule__title">My Schedule</h1>
+                
+            <h1 className="schedule__title">My Schedule</h1>
+            <Gradient/>
             </div>
         )
     }
