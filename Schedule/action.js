4d437322e6bee85554d27b2a36bfd03b46331a4c
Emily433 <emilypmortensen@gmail.com>
2018-07-17 09:29:11 -0600

    recent updates

--- Schedule/action.js
+++ Schedule/action.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
-import Icon from './icon';
+import React, { Component } from "react";
+import Icon from "./icon";
 
 class Action extends Component {
   render() {
     return (
-      <div className={'${this.props.className} action'}>
-        { Icon('fas fa-plus-circle') }
+      <div className={"${this.props.className} action"}>
+        {Icon("fas fa-plus-circle")}
         {/* fas fa-times-circle */}
       </div>
-    )
+    );
   }
 }
+toggleArrow = function() {
+  this.props.callback(this.state.status);
+  if (this.state.status) {
+    document.getElementById(this.id).classList.remove("arrow-closed");
+  } else {
+    document.getElementById(this.id).classList.add("arrow-closed");
+  }
+
+  this.setState({ status: !this.state.status });
+}.bind(this);
+
 export default Arrow;
-<button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-</button>
+<button
+  onClick={() => this.props.onClick()}
+  className={`${this.props.className} action`}
+/>;
