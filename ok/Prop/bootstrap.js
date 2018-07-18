0d80efe2d482b7721e24feab85e9776b7022364b
Emily433 <emilypmortensen@gmail.com>
2018-07-18 09:38:27 -0600

    nice

---
+++ ok/Prop/bootstrap.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+      <Switch >
+      <Route path='/' component={Signin}/>
+      <Route path='/' component={Signin}/>
+    </Switch>
+      </BrowserRouter>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
