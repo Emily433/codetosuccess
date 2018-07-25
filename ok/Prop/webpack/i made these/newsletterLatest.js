4fcee29c9596746b551ae289def640d3b30a5095
Emily433 <emilypmortensen@gmail.com>
2018-07-25 11:39:09 -0600

    working on finishing the newsletter

--- ok/Prop/webpack/i made these/newsletterLatest.js
+++ ok/Prop/webpack/i made these/newsletterLatest.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
+function Button({className, callback, text, icon}) {
+    if(icon) {
+        return (
+            <a onClick={callback} className={`${classname} button`}>
+            <i className={icon}></i>
+        </a>  
+
+        )
+    } 
+}
 class NewsletterLatest extends Component {
+
+    handleEdit = () => {
+        console.log('trying to handle edit');
+    }
+
     render() {
+        const { title, imageUrl, body } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest_title'>Title goes here</h1>
-                <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img className='newsletter-latest__image' src={imageUrl}/>
+                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/> 
                 <div className='newsletter-latest__body'>
-                    <p>Lorem Ipsum</p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
     }
 }
-
-export default NewsletterLatest;
